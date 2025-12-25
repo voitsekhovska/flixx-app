82a238de0022ccd75219f747327913aa4d89ff76
ye.voitsekhovska <sunakojane6@gmail.com>
2025-12-26 01:00:43 +0200

    added popular movies and tv pages

---
+++ js/script.js
@@ -0,0 +1,132 @@
+const global = {
+  currentPage: window.location.pathname,
+};
+
+// fetching data from TMDB API
+const fetchAPIData = async (endpoint) => {
+  const API_KEY = "3f8fde3fd0decdbfe7525e780646eed0";
+  const API_URL = "https://api.themoviedb.org/3/";
+
+  showSpinner();
+
+  const response = await fetch(
+    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
+  );
+  const data = await response.json();
+
+  hideSpinner();
+  return data;
+};
+
+// spinner
+const showSpinner = () => {
+  document.querySelector(".spinner").classList.add("show");
+};
+
+const hideSpinner = () => {
+  document.querySelector(".spinner").classList.remove("show");
+};
+
+// popular movies
+const displayPopularMovies = async () => {
+  const { results } = await fetchAPIData("movie/popular");
+
+  results.forEach((movie) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `<a href="movie-details.html?id=${movie.id}">
+            ${
+              movie.poster_path
+                ? `<img
+              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              class="card-img-top"
+              alt="${movie.title}"
+            />`
+                : `<img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${movie.title}"
+            />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${movie.release_date}</small>
+            </p>
+          </div>`;
+
+    document.querySelector("#popular-movies").appendChild(div);
+  });
+};
+
+// popular tv shows
+const displayPopularShows = async () => {
+  const { results } = await fetchAPIData("tv/popular");
+
+  results.forEach((show) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `<a href="tv-details.html?id=${show.id}">
+            ${
+              show.poster_path
+                ? `<img
+              src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+              class="card-img-top"
+              alt="${show.name}"
+            />`
+                : `<img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${show.name}"
+            />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${show.name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Aired: ${show.first_air_date}</small>
+            </p>
+          </div>`;
+
+    document.querySelector("#popular-shows").appendChild(div);
+  });
+};
+
+// Highlight active nav links
+const highlightActiveLink = () => {
+  const navLinks = document.querySelectorAll(".nav-link");
+
+  navLinks.forEach((link) => {
+    if (link.getAttribute("href") === global.currentPage) {
+      link.classList.add("active");
+    }
+  });
+};
+
+// Init app
+// vanilla router
+const init = () => {
+  switch (global.currentPage) {
+    case "/":
+    case "/index.html":
+      displayPopularMovies();
+      break;
+    case "/movie-details.html":
+      console.log("Movie details");
+      break;
+    case "/search.html":
+      console.log("Search");
+      break;
+    case "/shows.html":
+      displayPopularShows();
+      break;
+    case "/tv-details.html":
+      console.log("TV details");
+      break;
+  }
+
+  highlightActiveLink();
+};
+
+document.addEventListener("DOMContentLoaded", init);
